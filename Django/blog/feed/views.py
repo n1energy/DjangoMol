2f9a2c7e9e84145d926247660767325e2c87e4fc
piwi <n1energy@mail.ru>
2021-10-14 16:04:43 +1000

    complete lesson 4

--- Django/blog/feed/views.py
+++ Django/blog/feed/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+def posts_list(request):
+    n = ['masha','olya','elena']
+    return render(request,'feed/index.html', context = {'names' : n })
 # Create your views here.
